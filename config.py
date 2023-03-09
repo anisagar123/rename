1bb358062346b39353642bc8939e3315fe07202d
Kushal HK <115166395+Kushalhk@users.noreply.github.com>
2023-03-09 23:55:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ FORCE_SUB = os.environ.get("FORCE_SUB", "TG_UPDATES1")
 
 DB_NAME = os.environ.get("DB_NAME","Cluster0")     
 
-DB_URL = os.environ.get("DB_URL","mongodb+srv://Rename:Rename@cluster0.vlfzuid.mongodb.net/?retryWrites=true&w=majority")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://King:King@cluster0.nuc8bhf.mongodb.net/?retryWrites=true&w=majority")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
